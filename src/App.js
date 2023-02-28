691a19e8f8e7977010ff18fafb5452463a3ac2c8
niels <Niels.L.Larsen-1@ou.edu>
2023-02-28 10:45:01 -0600

    finishing touches on skeleton code

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-import { useAuthState } from 'react-firebase-hooks/auth';
+import { useAuthState, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 firebase.initializeApp({
@@ -26,15 +26,46 @@ firebase.initializeApp({
 })
 const auth = firebase.auth();
 const firestore = firebase.firestore();
-
+const [] = useAuthState(auth);
 function App() {
   return (
     <div className="App">
       <header className="App-header">
        
       </header>
+      <section >
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
     </div>
   );
 }
+function signIn(){
+const SignInWithGoogle = () => {const provider = new firebase.auth.GoogleAuthProvider();
+auth.signInWithPopup(provider);}
+return (<button onClick={SignInWithGoogle}> Sign in with Boogle</button>)
 
+
+
+}
+function signOut(){
+  return auth.currentUSer && (<button onClick={() => auth.signOut()}>Sign Out</button>)
+}
+
+function ChatRoom() {
+const messageRef = firestore.collection('messages');
+const query = messageRef.orderBy('createdAt').limit(25);
+const [messages] = useCollectionData(query, {idField: 'id'});
+return(
+  <>
+  <div>
+    {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+  </div>
+  </>
+
+)
+}
+function ChatMessage(props) {
+  const { text, uid } = props.message;
+  return <p>{text}</p>
+}
 export default App;

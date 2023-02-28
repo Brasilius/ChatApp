c9c414aa0495f15441b3855138877e63601ce499
niels <Niels.L.Larsen-1@ou.edu>
2023-02-28 10:13:10 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAlmPmE9d9CLhH7R1hPaO0yi_u55TNW4NQ",
+
+  authDomain: "basic-chat-438fd.firebaseapp.com",
+
+  projectId: "basic-chat-438fd",
+
+  storageBucket: "basic-chat-438fd.appspot.com",
+
+  messagingSenderId: "704598221642",
+
+  appId: "1:704598221642:web:4787c333f151995692d7b3",
+
+  measurementId: "G-8YZE6H3MJL"
+
+  //configurations
+})
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
     </div>
   );
